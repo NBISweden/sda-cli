4840db3b82385a8f02a53478c6eb93e21a163e06
Martin Norling <norlingjr@gmail.com>
2022-01-27 15:28:29 +0100

    Add decryption function to decrypt

--- decrypt/decrypt.go
+++ decrypt/decrypt.go
@@ -4,11 +4,14 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/NBISweden/sda-cli/helpers"
 	"github.com/elixir-oslo/crypt4gh/keys"
+	"github.com/elixir-oslo/crypt4gh/streaming"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -48,8 +51,17 @@ func Decrypt(args []string) error {
 		return fmt.Errorf("argument parsing failed, reason: %v", err)
 	}
 
+	// format input and output files
 	// Args() returns the non-flag arguments, which we assume are filenames.
-	files := Args.Args()
+	// All filenames are read into a struct together with their output filenames
+	files := []helpers.EncryptionFileSet{}
+	for _, filename := range Args.Args() {
+
+		// Set directory for the output file
+		unencryptedFilename := strings.TrimSuffix(filename, ".c4gh")
+
+		files = append(files, helpers.EncryptionFileSet{Encrypted: filename, Unencrypted: unencryptedFilename})
+	}
 
 	// Create a private key and exit if the -createKey flag is set
 	// Currently, no password can be provided.
@@ -67,12 +79,21 @@ func Decrypt(args []string) error {
 	}
 
 	// Loading private key file
-	_, err = readPrivateKey(*privateKeyFile, "")
+	privateKey, err := readPrivateKey(*privateKeyFile, "")
 	if err != nil {
 		return err
 	}
 
-	log.Infof("Encrypting files %s with key %s", files, *privateKeyFile)
+	// decrypt the input files
+	numFiles := len(files)
+	for i, file := range files {
+		log.Infof("Decrypting file %v/%v: %s", i+1, numFiles, file.Encrypted)
+
+		err = decrypt(file.Encrypted, file.Unencrypted, *privateKey)
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
@@ -155,4 +176,48 @@ func readPrivateKey(filename, password string) (key *[32]byte, err error) {
 	privateKey, err := keys.ReadPrivateKey(file, []byte(password))
 
 	return &privateKey, err
+}
+
+// decrypts the data in `filename` with the given `privateKey`, writing the
+// resulting data to `outfile`.
+func decrypt(filename, outfileName string, privateKey [32]byte) error {
+
+	// check that the infile exists, and the the outfile doesn't exist
+	if !helpers.FileIsReadable(filename) {
+		return fmt.Errorf("infile %s does not exist or could not be read", filename)
+	}
+
+	if helpers.FileExists(outfileName) {
+		return fmt.Errorf("outfile %s already exists", outfileName)
+	}
+
+	// open input file for reading
+	inFile, err := os.Open(filepath.Clean(filename))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := inFile.Close(); err != nil {
+			log.Errorf("error closing file: %s\n", err)
+		}
+	}()
+
+	// Create crypt4gh reader
+	crypt4GHReader, err := streaming.NewCrypt4GHReader(inFile, privateKey, nil)
+	if err != nil {
+		return fmt.Errorf("could not create cryp4gh reader: %s", err)
+	}
+
+	// open output file for writing
+	outFile, err := os.Create(filepath.Clean(outfileName))
+	if err != nil {
+		return fmt.Errorf("could not create output file %s: %s", outfileName, err)
+	}
+
+	_, err = io.Copy(outFile, crypt4GHReader)
+	if err != nil {
+		return fmt.Errorf("could not decrypt file %s: %s", filename, err)
+	}
+
+	return nil
 }
--- decrypt/decrypt_test.go
+++ decrypt/decrypt_test.go
@@ -2,20 +2,23 @@ package decrypt
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/NBISweden/sda-cli/encrypt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
 type DecryptTests struct {
 	suite.Suite
-	tempDir  string
-	testFile *os.File
+	tempDir     string
+	fileContent []byte
+	testFile    *os.File
 }
 
 func TestDecryptTestSuite(t *testing.T) {
@@ -32,13 +35,17 @@ func (suite *DecryptTests) SetupTest() {
 		log.Fatal("Couldn't create temporary test directory", err)
 	}
 
-	// create an existing test file with some known content
+	// create a test file...
 	suite.testFile, err = ioutil.TempFile(suite.tempDir, "testfile-")
 	if err != nil {
 		log.Fatal("cannot create temporary public key file", err)
 	}
 
-	err = ioutil.WriteFile(suite.testFile.Name(), []byte("content"), 0600)
+	// ... create some content ...
+	suite.fileContent = []byte("This is some fine content right here.")
+
+	// ... and write the known content to it
+	err = ioutil.WriteFile(suite.testFile.Name(), suite.fileContent, 0600)
 	if err != nil {
 		log.Fatalf("failed to write to testfile: %s", err)
 	}
@@ -90,4 +97,70 @@ func (suite *DecryptTests) TestreadPrivateKey() {
 	// Test reading a real key
 	_, err = readPrivateKey(fmt.Sprintf("%s.sec.pem", testKeyFile), "")
 	assert.NoError(suite.T(), err)
+}
+
+func (suite *DecryptTests) Testdecrypt() {
+
+	testKeyFile := filepath.Join(suite.tempDir, "testkey")
+	encryptedFile := fmt.Sprintf("%s.c4gh", suite.testFile.Name())
+	decryptedFile := filepath.Join(suite.tempDir, "decrypted_file")
+
+	// generate key files
+	err := generateKeyPair(testKeyFile, "")
+	if err != nil {
+		log.Fatalf("couldn't generate testing key pair: %s", err)
+	}
+	// and read the private key
+	privateKey, err := readPrivateKey(fmt.Sprintf("%s.sec.pem", testKeyFile), "")
+	if err != nil {
+		log.Fatalf("couldn't read test key: %s", err)
+	}
+
+	// Encrypt a file using the encrypt module. change to the test directory to
+	// make sure that the checksum files end up there.
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("could not get working directory")
+	}
+	err = os.Chdir(suite.tempDir)
+	if err != nil {
+		log.Fatal("could not change into test directory")
+	}
+	encryptArgs := []string{"-key", fmt.Sprintf("%s.pub.pem", testKeyFile), suite.testFile.Name()}
+	err = encrypt.Encrypt(encryptArgs)
+	if err != nil {
+		log.Fatalf("couldn't encrypt file for decryption test: %s", err)
+	}
+	err = os.Chdir(cwd)
+	if err != nil {
+		log.Fatal("could not return from test directory")
+	}
+
+	// Test decrypting a non-existent file
+	err = decrypt(filepath.Join(suite.tempDir, "non-existent"), "output_file", *privateKey)
+	assert.EqualError(suite.T(), err, fmt.Sprintf("infile %s does not exist or could not be read", filepath.Join(suite.tempDir, "non-existent")))
+
+	// Test decrypting where the output file exists
+	err = decrypt(encryptedFile, suite.testFile.Name(), *privateKey)
+	assert.EqualError(suite.T(), err, fmt.Sprintf("outfile %s already exists", suite.testFile.Name()))
+
+	// Test decryption with malformed key
+	fakeKey := [32]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	err = decrypt(encryptedFile, decryptedFile, fakeKey)
+	assert.EqualError(suite.T(), err, "could not create cryp4gh reader: could not find matching public key header, decryption failed")
+
+	// Test decrypting with the real key
+	err = decrypt(encryptedFile, decryptedFile, *privateKey)
+	assert.NoError(suite.T(), err)
+
+	// Check content of the decrypted file
+	inFile, err := os.Open(decryptedFile)
+	if err != nil {
+		log.Fatalf("Couldn't open decrypted file %s for content checking", decryptedFile)
+	}
+	fileData, err := io.ReadAll(inFile)
+	if err != nil {
+		log.Fatal("Couldn't read decrypted filedata for content checking")
+	}
+	assert.Equal(suite.T(), fileData, suite.fileContent)
 }
