468e67ec1d0912e8420bf5a8a16ac21fe78dea02
Alex Aperis <aperisalex@gmail.com>
2022-03-24 15:07:09 +0100

    correct error string format

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -195,12 +195,12 @@ func checkFiles(files []helpers.EncryptionFileSet) error {
 	for _, file := range files {
 		// check that the input file exists and is readable
 		if !helpers.FileIsReadable(file.Unencrypted) {
-			return fmt.Errorf("Cannot read input file %s", file.Unencrypted)
+			return fmt.Errorf("cannot read input file %s", file.Unencrypted)
 		}
 
 		// check that the output file doesn't exist
 		if helpers.FileExists(file.Encrypted) {
-			return fmt.Errorf("Outfile %s already exists", file.Encrypted)
+			return fmt.Errorf("outfile %s already exists", file.Encrypted)
 		}
 
 		// Check if the input file is already encrypted
@@ -218,10 +218,10 @@ func checkFiles(files []helpers.EncryptionFileSet) error {
 		magicWord := make([]byte, 8)
 		_, err = unEncryptedFile.Read(magicWord)
 		if err != nil {
-			return fmt.Errorf("Error reading input file %s, reason: %v", file.Unencrypted, err)
+			return fmt.Errorf("error reading input file %s, reason: %v", file.Unencrypted, err)
 		}
 		if string(magicWord) == "crypt4gh" {
-			return fmt.Errorf("Input file %s is already encrypted(.c4gh) - make sure the right pk was used", file.Unencrypted)
+			return fmt.Errorf("input file %s is already encrypted(.c4gh) - make sure the right pk was used", file.Unencrypted)
 		}
 	}
 
