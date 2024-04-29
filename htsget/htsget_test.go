0ff48c90d90d5d569a5c9664ceaa91de7bb8a8d2
Panos Chatzopoulos <panos.chatzopoulos@nbis.se>
2024-04-29 08:45:45 +0200

    Added test file

---
+++ htsget/htsget_test.go
@@ -0,0 +1,28 @@
+package htsget
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestSuite struct {
+	suite.Suite
+	tempDir     string
+	fileContent []byte
+	testFile    *os.File
+}
+
+func TestConfigTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+func (suite *TestSuite) TestNoArgument() {
+
+	os.Args = []string{"htsget"}
+
+	err := Htsget(os.Args)
+	assert.EqualError(suite.T(), err, "missing required arguments, datasetID, filename, htsgethost and key are required")
+}
