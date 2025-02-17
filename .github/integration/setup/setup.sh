6046d89d6a488e83e92f5e12fb9f00461bb05687
Alex Aperis <aperisalex@gmail.com>
2025-02-17 12:55:50 +0100

    cleanup tests

--- .github/integration/setup/setup.sh
+++ .github/integration/setup/setup.sh
@@ -63,7 +63,7 @@ do echo "waiting for s3 to become ready"
 done
 
 RETRY_TIMES=0
-until docker ps -f name="proxy" --format "{{.Status}}" | grep "Up About"
+until docker ps -f name="proxy" --format "{{.Status}}" | grep "Up "
 do echo "waiting for proxy to become ready"
     RETRY_TIMES=$((RETRY_TIMES+1));
     if [ "$RETRY_TIMES" -eq 30 ]; then
