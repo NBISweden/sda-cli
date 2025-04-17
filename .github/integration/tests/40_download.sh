e3b3937f28d7c014a728f6fbf6b67ded7813fac8
Joakim Bygdell <joakim.bygdell@nbis.se>
2025-04-17 12:05:22 +0200

    Fix tests

--- .github/integration/tests/40_download.sh
+++ .github/integration/tests/40_download.sh
@@ -10,17 +10,21 @@ else
 fi
 
 # Download file by using the sda-cli download command
-./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir test-download main/subfolder/dummy_data.c4gh
-
-C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem test-download/main/subfolder/dummy_data.c4gh
+./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir test-download main_folder/subfolder/dummy_data.c4gh
+if [ ! -f "test-download/main_folder/subfolder/dummy_data.c4gh" ]; then
+    echo "Downloaded file not found for first test"
+    ls -r test-download
+    exit 1
+fi
+C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem test-download/main_folder/subfolder/dummy_data.c4gh
 # Check if file exists in the path
-if [ ! -f "test-download/main/subfolder/dummy_data" ]; then
-    echo "Downloaded file not found"
+if [ ! -f "test-download/main_folder/subfolder/dummy_data" ]; then
+    echo "Downloaded file not found for first test"
     exit 1
 fi
 
 # Check the first line of that file
-first_line=$(head -n 1 test-download/main/subfolder/dummy_data)
+first_line=$(head -n 1 test-download/main_folder/subfolder/dummy_data)
 if [[ $first_line != *"THIS FILE IS JUST DUMMY DATA"* ]]; then
     echo "First line does not contain the expected string"
     exit 1
@@ -31,7 +35,7 @@ rm -r test-download
 # Download whole dataset by using the sda-cli download command
 ./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem  -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir download-dataset --dataset
 
-filepaths="download-dataset/main/subfolder/dummy_data download-dataset/main/subfolder2/dummy_data2 download-dataset/main/subfolder2/random/dummy_data3"
+filepaths="download-dataset/main_folder/subfolder/dummy_data download-dataset/main_folder/subfolder2/dummy_data2 download-dataset/main_folder/subfolder2/random/dummy_data3"
 
 # Check if all the files of the dataset have been downloaded
 for filepath in $filepaths; do
@@ -44,18 +48,18 @@ done
 rm -r download-dataset
 
 # Download encrypted file by using the sda-cli download command
-./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir test-download main/subfolder/dummy_data.c4gh
+./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir test-download main_folder/subfolder/dummy_data.c4gh
 
 # Check if file exists in the path
-if [ ! -f "test-download/main/subfolder/dummy_data.c4gh" ]; then
+if [ ! -f "test-download/main_folder/subfolder/dummy_data.c4gh" ]; then
     echo "Downloaded file not found"
     exit 1
 fi
 
 # Decrypt the downloaded file
-C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem test-download/main/subfolder/dummy_data.c4gh
+C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem test-download/main_folder/subfolder/dummy_data.c4gh
 
-if [ -f test-download/main/subfolder/dummy_data  ]; then
+if [ -f test-download/main_folder/subfolder/dummy_data  ]; then
     echo "Decrypting downloaded file succeeded"
 else
     echo "Failed to decrypt downloaded file"
@@ -63,7 +67,7 @@ else
 fi
 
 # Check the first line of that file
-first_line=$(head -n 1 test-download/main/subfolder/dummy_data)
+first_line=$(head -n 1 test-download/main_folder/subfolder/dummy_data)
 if [[ $first_line != *"THIS FILE IS JUST DUMMY DATA"* ]]; then
     echo "First line does not contain the expected string"
     exit 1
@@ -71,9 +75,9 @@ fi
 
 # Download recursively a folder
 echo "Downloading content of folder"
-./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir download-folder --recursive main/subfolder2
+./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir download-folder --recursive main_folder/subfolder2
 
-folderpaths="download-folder/main/subfolder2/dummy_data2 download-folder/main/subfolder2/random/dummy_data3"
+folderpaths="download-folder/main_folder/subfolder2/dummy_data2 download-folder/main_folder/subfolder2/random/dummy_data3"
 
 # Check if the content of the folder has been downloaded
 for folderpath in $folderpaths; do
@@ -89,14 +93,14 @@ rm -r download-folder
 ./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir download-fileid urn:neic:001-001
 
 # Check if file exists in the path
-if [ ! -f "download-fileid/main/subfolder/dummy_data.c4gh" ]; then
+if [ ! -f "download-fileid/main_folder/subfolder/dummy_data.c4gh" ]; then
     echo "Downloaded file by using the file id not found"
     exit 1
 fi
 
-C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem download-fileid/main/subfolder/dummy_data.c4gh
+C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem download-fileid/main_folder/subfolder/dummy_data.c4gh
 # Check the first line of the file
-first_line_id=$(head -n 1 download-fileid/main/subfolder/dummy_data)
+first_line_id=$(head -n 1 download-fileid/main_folder/subfolder/dummy_data)
 if [[ $first_line_id != *"THIS FILE IS JUST DUMMY DATA"* ]]; then
     echo "This is not the file with the given file id"
     exit 1
@@ -109,7 +113,7 @@ echo "Downloading content of a text file"
 ./sda-cli -config testing/s3cmd-download.conf download -pubkey user_key.pub.pem -dataset-id https://doi.example/ty009.sfrrss/600.45asasga -url http://localhost:8080 -outdir download-from-file --from-file testing/file-list.txt
 
 # Check if the content of the text file has been downloaded
-content_paths="download-from-file/main/subfolder/dummy_data.c4gh download-from-file/main/subfolder2/dummy_data2.c4gh"
+content_paths="download-from-file/main_folder/subfolder/dummy_data.c4gh download-from-file/main_folder/subfolder2/dummy_data2.c4gh"
 
 for content_path in $content_paths; do
     if [ ! -f "$content_path" ]; then
@@ -118,9 +122,9 @@ for content_path in $content_paths; do
     fi
 done
 
-C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem download-from-file/main/subfolder/dummy_data.c4gh
+C4GH_PASSWORD="" ./sda-cli decrypt -key user_key.sec.pem download-from-file/main_folder/subfolder/dummy_data.c4gh
 # Check the first line of the file
-first_line_file=$(head -n 1 download-from-file/main/subfolder/dummy_data)
+first_line_file=$(head -n 1 download-from-file/main_folder/subfolder/dummy_data)
 if [[ $first_line_file != *"THIS FILE IS JUST DUMMY DATA"* ]]; then
     echo "First line does not contain the expected string"
     exit 1
