236c2f7c5350c7d8ef1f3148888c53d31ebb08af
Karl Grönberg <karl.gronberg@nbis.se>
2025-08-20 10:14:26 +0200

    feat(helpers): merge if checks for validation of host base url after schema enforce with nil check instead of error check

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -261,10 +261,7 @@ func LoadConfigFile(path string) (*Config, error) {
 
 	// Parse URL again to validate that a host can be parsed after scheme has been enforced
 	u, err = url.Parse(config.HostBase)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse host base from configuration file, reason: %v", err)
-	}
-	if u.Host == "" {
+	if u == nil || u.Host == "" {
 		return nil, fmt.Errorf("failed to parse host base from configuration file, reason: a valid host can not be parsed")
 	}
 
