1336c2c0dcbb8246ba6d6b8adde4bf3202e1c789
zeidlitz <zeidlitzerik@gmail.com>
2025-10-09 11:38:22 +0200

    Addressing comments

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,8 +12,7 @@ var Version = "0-development"
 var rootCmd = &cobra.Command{
 	Use:   "sda-cli",
 	Short: "CLI tool used to interact with SDA",
-	Long: `Command line tool used to interact with the Sensitive Data Archive (SDA)
-	For more infomration, see the README at https://github.com/NBISweden/sda-cli/blob/main/README.md`,
+	Long:  `Command line tool used to interact with the Sensitive Data Archive (SDA)`,
 }
 
 func Execute() error {
@@ -23,7 +25,12 @@ func Execute() error {
 }
 
 func init() {
+	readmeURL := "https://github.com/NBISweden/sda-cli/blob/main/README.md"
+	if !strings.Contains(Version, "development") {
+		readmeURL = fmt.Sprintf("https://github.com/NBISweden/sda-cli/blob/%s/README.md", Version)
+	}
 	rootCmd.PersistentFlags().StringP("config", "c", "s3cmd.conf", "The configuration file for s3cmd")
+	rootCmd.Long += fmt.Sprintf("\nFor more information, see the README at: %s", readmeURL)
 }
 
 func AddCommand(command *cobra.Command) {
